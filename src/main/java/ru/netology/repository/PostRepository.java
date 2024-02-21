18fd3bfcce623c88edd2de656ca3e7eb683cef12
Yury Shakhmatov <shufflepkt@yandex.ru>
2024-02-21 13:21:17 +0100

    Dependency injection through annotation has been added

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
@@ -9,7 +10,7 @@ import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
-
+@Repository
 public class PostRepository {
   private final CopyOnWriteArrayList<Post> postRepository = new CopyOnWriteArrayList<>();
   private final AtomicLong counter = new AtomicLong();
